27218aab1ec18813fa969c6ddb5ece99aa9d7d7d
Ivetta <movette0903@gmail.com>
2023-06-22 11:56:06 +0300

    Initial commit

---
+++ tests/test.py
@@ -0,0 +1,21 @@
+import pytest
+
+from app.calc import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+    def test_adding_success(self):
+        assert self.calc.adding(self, 2, 3) == 5
+
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 3, 2) == 1
+
+    def test_division_success(self):
+        assert self.calc.division(self, 4, 2) == 2
+
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 5, 5) == 25
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
